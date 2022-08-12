42aa0d4c494f0fc304469c7169632dc317e6030c
shark-2000 <katyayns40@gmail.com>
2022-08-12 15:56:47 +0530

    integration of api testing

---
+++ src/main/java/utils/API_Initializer.java
@@ -0,0 +1,36 @@
+package utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class API_Initializer {
+	public static String baseURI() throws IOException {
+		File file = new File(System.getProperty("user.dir") + "/src/main/resources/configuration/api.properties");
+		FileInputStream fis = new FileInputStream(file);
+		Properties prop = new Properties();
+		prop.load(fis);
+
+		return prop.getProperty("baseURI");
+	}
+
+	public static String endPoint() throws IOException {
+		File file = new File(System.getProperty("user.dir") + "/src/main/resources/configuration/api.properties");
+		FileInputStream fis = new FileInputStream(file);
+		Properties prop = new Properties();
+		prop.load(fis);
+
+		return prop.getProperty("endPoint");
+	}
+
+	public static String objectKey() throws IOException {
+		File file = new File(System.getProperty("user.dir") + "/src/main/resources/configuration/api.properties");
+		FileInputStream fis = new FileInputStream(file);
+		Properties prop = new Properties();
+		prop.load(fis);
+
+		return prop.getProperty("objectkey");
+	}
+
+}
